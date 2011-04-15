d343eddb2f4b13528fadb91515a207481635e2d6
thugrigby <plutonoodle.com>
2011-04-15 16:45:25 -0700

    first commit. tutorial part 1

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require 'erb'
+
+get '/' do
+  erb :index
+end
+
+get '/the_resulting_action_from_clicking_submit' do
+  @instance_variable1 = params["this_is_what_gets_fed_into_params1"]
+  @instance_variable2 = params["this_is_what_gets_fed_into_params2"]
+  @instance_variable3 = params["this_is_what_gets_fed_into_params3"]
+  erb :result
+end
